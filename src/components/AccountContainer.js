ec2c3d533c71de649bfa66677d281d3c5dcfaa04
alexander <alexandernyaga1999@gmail.com>
2023-10-31 15:09:21 +0300

    Filter Transactions

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -49,7 +49,7 @@ function AccountContainer() {
             })
         }
 
-        //filter transactions
+        //filter transactions based on search in description
         filterSearchTransactions = () => {
             let {transactions, search, select} = this.state
 
@@ -58,6 +58,56 @@ function AccountContainer() {
             })
         }
 
+        //Switch statement to filter transaction based on different criteria
+        switch(select){
+            case "all" :
+              return filterSearch
+      
+            case "descriptionUP" : 
+              return filterSearch.sort( (wordA, wordB) => {
+                  return wordA.description.localeCompare(wordB.description)
+              })
+      
+            case "descriptionDOWN" : 
+            return filterSearch.sort( (wordA, wordB) => {
+                return wordB.description.localeCompare(wordA.description)
+            })
+              
+            case "categoryUP" : 
+            return filterSearch.sort( (wordA, wordB) => {
+                return wordA.category.localeCompare(wordB.category)
+            })
+      
+            case "categoryDOWN" : 
+            return filterSearch.sort( (wordA, wordB) => {
+                return wordB.category.localeCompare(wordA.category)
+            })
+      
+            case "amountUP" : 
+            return filterSearch.sort( (numA, numB) => {
+                return numA.amount - numB.amount
+            })
+      
+            case "amountDOWN" : 
+            return filterSearch.sort( (numA, numB) => {
+                return numB.amount - numA.amount
+            })
+      
+            case "dateUP" : 
+            return filterSearch.sort( (numA, numB) => {
+                return new Date(numA.date) - new Date(numB.date)
+            })
+      
+            case "dateDOWN" : 
+            return filterSearch.sort( (numA, numB) => {
+                return new Date(numB.date) - new Date(numA.date)
+            })
+      
+            default:
+          }
+        }
+      
+
 
 
 
@@ -65,7 +115,7 @@ function AccountContainer() {
 
     
 
-    }
+    
 
     export default AccountContainer;
 
