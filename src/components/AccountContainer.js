89982dba28f26cbf87749909fc3d8247c98f07c3
alexander <alexandernyaga1999@gmail.com>
2023-10-31 14:38:21 +0300

    add account container

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,1 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import SearchBar from "SearchBar";
+
+function AccountContainer() {
+        state = {Transactions : [], SearchBar: "", select: "all"}
+
+        useEffect(() => {
+            fetch('http://localhost:6001/transactions')
+            .then(r => r.json())
+            .then(resp => {
+                this.setstate({
+                    transactions:resp
+                })
+            })
+        })
+
+        addTransactionFun = (addTransaction) => {
+            this.setstate(prevState => {
+                return {
+                    transactions: [...prevState.transactions, addTransaction]
+                }
+            })
+        }
+
+
+    
+
+    
+
+    }
+
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import AccountContainer from AccountContainer
 import './App.css';
+import AccountContainer from './AccountContainer'
 
 function App() {
   return (
