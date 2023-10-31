954b9789bf9826082ea25fac06a51f9014c35a9b
alexander <alexandernyaga1999@gmail.com>
2023-10-31 12:17:04 +0300

    import react

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AccountContainer from AccountContainer
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>Bank of Flatiron</h2>
+    </div>
+    <AccountContainer />
+    </div>
+  );
+}
+
+export default App;
