afae9e9a2980b84aa576ae895e76d5574f96a4fb
alexander <alexandernyaga1999@gmail.com>
2023-11-01 04:44:28 +0300

    Handle Submit button

---
+++ src/components/AddTransactionForm.js
@@ -0,0 +1,37 @@
+import React, {Component} from "react";
+
+//class function
+state = { date:"", description:"", category:"",amount:""}
+
+handleSubmit = (evt) => {
+    evt.preventDefault()
+    fetch('http://localhost:6001/transactions', {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        date: this.state.date,
+        description: this.state.description,
+        category: this.state.category,
+        amount: this.state.amount
+      })
+    })
+    .then(r => r.json())
+    .then(addTransaction => {
+      this.props.addTransactionFun(addTransaction)
+      this.setState({
+        date: "",
+        description: "",
+        category: "",
+        amount: ""
+      })
+    })
+
+}
+
+
+
+
+
+      
