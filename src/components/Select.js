e7c3a01c9966a2e7ed1551afc7df4f30f122a908
alexander <alexandernyaga1999@gmail.com>
2023-11-01 04:59:20 +0300

    Select choose options

--- src/components/Select.js
+++ src/components/Select.js
@@ -6,3 +6,22 @@ const Select = (props) => {
         props.selectFun(evt.terget.value)
     }
 }
+
+return (
+    <div>
+      <select value={props.select} onChange={handleSelect}>
+         <option value={"all"}>All</option>
+         <option value={"descriptionUP"}>Sort by description (ASC)</option>
+         <option value={"descriptionDOWN"}>Sort by description (DESC)</option>
+         <option value={"categoryUP"}>Sort by category (ASC)</option>
+         <option value={"categoryDOWN"}>Sort by category (DESC)</option>
+         <option value={"amountUP"}>Sort by amount (ASC)</option>
+         <option value={"amountDOWN"}>Sort by amount (DESC)</option>
+         <option value={"dateUP"}>Sort by date (ASC)</option>
+         <option value={"dateDOWN"}>Sort by date (DESC)</option>
+      </select>
+    </div>
+   )
+ 
+ 
+ export default Select;
