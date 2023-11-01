e36fbfa5d968cfa79abb09fabe34e81c5bf3e94e
alexander <alexandernyaga1999@gmail.com>
2023-11-01 05:27:20 +0300

    return the table rendered entry

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -9,7 +9,32 @@ const TransactionList = (props) => {
         transaction={transactionObj}
         deleteTransactionFun={props.deleteTransactionFun}
           />
-                
-    })}
+    }
+    
 
+    return (
+        <table className="ui celled striped padded table">
+          <tbody>
+            <tr>
+              <th>
+                <h3 className="ui center aligned header">Date</h3>
+              </th>
+              <th>
+                <h3 className="ui center aligned header">Description</h3>
+                < Select select={props.select} selectFun={props.selectFun}/>
+              </th>
+              <th>
+                <h3 className="ui center aligned header">Category</h3>
+              </th>
+              <th>
+                <h3 className="ui center aligned header">Amount</h3>
+              </th>
+            </tr>
+            {componentArray}
+          </tbody>
+        </table>
+      );
+    };
+    
+    export default TransactionsList;
     
