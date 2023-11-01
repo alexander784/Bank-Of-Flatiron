1ba82f0d1b8615c15533ce17fb3ceb52e60e5e8b
alexander <alexandernyaga1999@gmail.com>
2023-11-01 04:48:59 +0300

    render transaction component

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -36,6 +36,53 @@ handleChange = (evt) => {
     })
 }
 
+render() {
+
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.handleSubmit}>
+          <div className="inline fields">
+            <input 
+              type="date" 
+              name="date" 
+              value={this.state.date}
+              onChange={this.handleChange}
+            />
+            <input 
+              type="text" 
+              name="description" 
+              placeholder="Description" 
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
+            <input 
+              type="text" 
+              name="category" 
+              placeholder="Category" 
+              value={this.state.category}
+              onChange={this.handleChange}
+            />
+            <input
+              type="number"
+              name="amount"
+              placeholder="Amount"
+              step="0.01"
+              value={this.state.amount}
+              onChange={this.handleChange}
+            />
+          </div>
+          <button className="ui button" type="submit" > 
+            Add Transaction
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default addTransactionForm;
+
+
 
 
 
