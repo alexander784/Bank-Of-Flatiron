4dbee15f0e1b47c7243c3424d3d35d7b4bcf54f9
alexander <alexandernyaga1999@gmail.com>
2023-11-01 05:04:24 +0300

    Return the delete transact table

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -12,6 +12,20 @@ let handledelete = (evt) => {
     .then(deletedTransaction => {
       props.deleteTransactionFun(props.transaction)
     })
+
   }
+
+  return (
+    <tr>
+      <td>{date}</td>
+      <td>{description}</td>
+      <td>{category}</td>
+      <td>{amount}</td>
+      <td><button onClick={handledelete}>X</button></td> 
+    </tr>
+  );
+};
+
+export default Transaction;
 
 }
